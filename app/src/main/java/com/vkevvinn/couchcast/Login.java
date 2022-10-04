3f87f5a2e9f53c570a54948349b1b09b48e5318e
nooralyasiri <noor.alyasiri@mavs.uta.edu>
2022-10-04 14:58:11 -0500

    registration ui
    -- temporary splash screen
    -- registration UI
    -- login UI changes
    -- connect screens

--- app/src/main/java/com/vkevvinn/couchcast/Login.java
+++ app/src/main/java/com/vkevvinn/couchcast/Login.java
@@ -1,6 +1,9 @@
 package com.vkevvinn.couchcast;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,18 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView NEW_ACCOUNT;
+
+
+        NEW_ACCOUNT = findViewById(R.id.textView5);
+        NEW_ACCOUNT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent downloadIntent = new Intent(getApplicationContext(), Registration.class);
+                startService(downloadIntent);
+                startActivity(new Intent(Login.this, Registration.class));
+            }
+        });
     }
 }
