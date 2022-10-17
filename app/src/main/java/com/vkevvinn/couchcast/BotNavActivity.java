cf4f18583fed367572265a2a89cd2eb8cf6f913b
kevvinn <vukevinh02@gmail.com>
2022-10-16 19:46:39 -0500

    wip bottom nav bar

---
+++ app/src/main/java/com/vkevvinn/couchcast/BotNavActivity.java
@@ -0,0 +1,14 @@
+package com.vkevvinn.couchcast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BotNavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bot_nav);
+    }
+}
