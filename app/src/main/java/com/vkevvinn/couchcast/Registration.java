a0769b2fb8720a2e639127fbc720c3696a9c85d2
kevvinn <vukevinh02@gmail.com>
2022-10-18 14:46:24 -0500

    linked registration button to login screen

--- app/src/main/java/com/vkevvinn/couchcast/Registration.java
+++ app/src/main/java/com/vkevvinn/couchcast/Registration.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Registration extends AppCompatActivity {
@@ -16,9 +17,20 @@ public class Registration extends AppCompatActivity {
 
         TextView Login_Back;
 
+        Button registerbutton;
 
         Login_Back = findViewById(R.id.loginReturn2);
         Login_Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent downloadIntent = new Intent(getApplicationContext(), Login.class);
+                startService(downloadIntent);
+                startActivity(new Intent(Registration.this, Login.class));
+            }
+        });
+
+        registerbutton = findViewById(R.id.registerbutton);
+        registerbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent downloadIntent = new Intent(getApplicationContext(), Login.class);
