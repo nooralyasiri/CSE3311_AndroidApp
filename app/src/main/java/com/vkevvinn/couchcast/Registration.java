c2f26dc5a9eae64047f866be36e84c39fdbf525b
nooralyasiri <noor.alyasiri@mavs.uta.edu>
2022-11-01 12:17:05 -0500

    register button
    -- button to redirect user to log in after registration

--- app/src/main/java/com/vkevvinn/couchcast/Registration.java
+++ app/src/main/java/com/vkevvinn/couchcast/Registration.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Registration extends AppCompatActivity {
@@ -15,6 +16,7 @@ public class Registration extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
 
         TextView Login_Back;
+        Button registerbutton;
 
 
         Login_Back = findViewById(R.id.loginReturn2);
@@ -26,5 +28,18 @@ public class Registration extends AppCompatActivity {
                 startActivity(new Intent(Registration.this, Login.class));
             }
         });
+        registerbutton = findViewById(R.id.registerbutton);
+        registerbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent downloadIntent = new Intent(getApplicationContext(), Login.class);
+                startService(downloadIntent);
+                startActivity(new Intent(Registration.this, Login.class));
+            }
+        });
+
+
+
+
     }
 }
