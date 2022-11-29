a6320cd442aff51e56af6e14d30720dbe4fe00c3
kevvinn <vukevinh02@gmail.com>
2022-11-29 17:24:14 -0600

    first draft search functionality with recyclerview

--- app/src/main/java/com/vkevvinn/couchcast/SearchFragment.java
+++ app/src/main/java/com/vkevvinn/couchcast/SearchFragment.java
@@ -1,21 +1,34 @@
 package com.vkevvinn.couchcast;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import info.movito.themoviedbapi.TmdbApi;
+import info.movito.themoviedbapi.Utils;
+import info.movito.themoviedbapi.model.tv.TvSeries;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SearchFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment implements ShowlistRecyclerViewAdapter.ItemClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +38,14 @@ public class SearchFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    RecyclerView search_rv;
+    private ShowlistRecyclerViewAdapter adapter;
+    String apiKey = "4bb376189becc0b82f734fd11af958a0";
+    private List<TvSeries> trendingShows;
+    private ArrayList<Integer> showIds = new ArrayList<>();
+    private ArrayList<String> showNames = new ArrayList<>();
+    private ArrayList<String> posterUrls = new ArrayList<>();
 
     public SearchFragment() {
         // Required empty public constructor
@@ -62,14 +83,86 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search, container, false);
-        Button showInfoButton = view.findViewById(R.id.showInfoButton);
+
+        search_rv = view.findViewById(R.id.search_rv);
+        search_rv.setHasFixedSize(true);
+        LinearLayoutManager trendingLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        search_rv.setLayoutManager(trendingLayoutManager);
+        adapter = new ShowlistRecyclerViewAdapter(getActivity(), showIds, showNames, posterUrls);
+        adapter.setClickListener(this);
+        search_rv.setAdapter(adapter);
+
+
+        Button showInfoButton = view.findViewById(R.id.search_button);
+        EditText searchBox = view.findViewById(R.id.searchView);
+
         showInfoButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getActivity(), ShowInfo.class));
+                GetShowFromSearchTask getShowFromSearchTask = new GetShowFromSearchTask();
+                getShowFromSearchTask.execute(searchBox.getText().toString());
+//                startActivity(new Intent(getActivity(), ShowInfo.class));
             }
         });
         return view;
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(getActivity(), "You clicked " + adapter.getName(position) + " (Show ID " + adapter.getId(position) + ") on item position " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    private class GetShowFromSearchTask extends AsyncTask<String, Void, List<TvSeries>> {
+
+        @Override
+        protected List<TvSeries> doInBackground(String... query) {
+            TmdbApi tmdbApi = new TmdbApi(apiKey);
+            List<TvSeries> showQuery = tmdbApi.getSearch().searchTv(query[0],"en",0).getResults();
+            return showQuery;
+        }
+
+        @Override
+        protected void onPostExecute(List<TvSeries> showQuery) {
+            try {
+                showNames.clear();
+                showIds.clear();
+                posterUrls.clear();
+                trendingShows = showQuery;
+                for(TvSeries tvSeries : showQuery) {
+
+                    showNames.add(tvSeries.getName());
+                    showIds.add(tvSeries.getId());
+                    String posterPath = tvSeries.getPosterPath();
+                    GetPosterImage getPosterImage = new GetPosterImage();
+                    getPosterImage.execute(posterPath);
+                }
+            }
+
+            catch (Exception e) {
+                Toast.makeText(getActivity(), "Sorry, no popular shows found!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private class GetPosterImage extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... posterPaths) {
+            TmdbApi tmdbApi = new TmdbApi(apiKey);
+            Utils utils = new Utils();
+            try{
+                return Utils.createImageUrl(tmdbApi, posterPaths[0], "w500").toString();
+            } catch (Exception e) {
+                return "https://i.pinimg.com/236x/96/e2/c9/96e2c9bd131c8ae9bb2b88fff69f9579.jpg";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String imageUrl) {
+            Log.e("imageUrl: ", imageUrl.replaceAll("http", "https"));
+            posterUrls.add(imageUrl.replaceAll("http://", "https://"));
+            adapter.notifyDataSetChanged();
+        }
     }
 }
